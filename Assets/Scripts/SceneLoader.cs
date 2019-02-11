2332baba062c3e69a21496ae73a4103a293c2093
phibh <buiphi6789@gmail.com>
2019-02-11 15:41:34 +0700

    Glitch Garden

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]float timeToWait = 4f;
+    int currentSecenIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSecenIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSecenIndex == 0)
+        {
+            StartCoroutine(LoadStartScreen());
+        }
+    }
+
+    IEnumerator LoadStartScreen()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSecenIndex+1);
+    }
+}
